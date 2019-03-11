8811daf3e2acddb2778e7191fe8018e2b2e4e069
joel-razor1 <joelabraham8857@gmail.com>
2019-03-11 16:48:36 +0530

    Completed till section 7

---
+++ Section 7/data-types.dart
@@ -0,0 +1,20 @@
+//basic data types of dart
+
+void main() {
+  int function(){
+    return 8*8;
+  }
+  print("The value of the function is ");
+  String name="1";
+  String name1="Bpond";
+  num age=13;
+  int num1=45;
+  double num2=23.23;
+  bool isTrue=true;
+  print(function());
+  const pi=3.14;
+  print(pi);
+  print("$name and $name1 are concatenated");
+  print(age+num1);
+  print(pi<age);
+}
